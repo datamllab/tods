15ad535e8dd6de9c331fb28e19a4ec68d35ffd75
lhenry15 <khlai037@gmail.com>
2020-09-09 17:39:50 -0500

    add converted NAB dataset and conversion script

--- examples/run_automl.py
+++ examples/run_automl.py
@@ -6,11 +6,11 @@ from tods.utils import generate_dataset_problem
 from tods.search import BruteForceSearch
 
 # Some information
-table_path = 'datasets/NAB/realTweets/labeled_Twitter_volume_IBM.csv' # The path of the dataset
-target_index = 2 # what column is the target
+#table_path = 'datasets/NAB/realTweets/labeled_Twitter_volume_GOOG.csv' # The path of the dataset
+#target_index = 2 # what column is the target
 
-#table_path = 'datasets/yahoo_sub_5.csv'
-#target_index = 6 # what column is the target
+table_path = 'datasets/yahoo_sub_5.csv'
+target_index = 6 # what column is the target
 #table_path = 'datasets/NAB/realTweets/labeled_Twitter_volume_IBM.csv' # The path of the dataset
 time_limit = 30 # How many seconds you wanna search
 #metric = 'F1' # F1 on label 1
