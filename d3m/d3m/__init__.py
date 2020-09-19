6c71b35fa7defd3eef50aaf7d56bbc39b1ea31ee
Daochen Zha <daochen.zha@tamu.edu>
2020-09-19 12:56:57 -0500

    Init

--- d3m/d3m/__init__.py
+++
@@ -1,8 +0,0 @@
-__version__ = '2020.5.18'
-__description__ = 'Common code for D3M project'
-__author__ = 'DARPA D3M Program'
-
-
-from d3m import namespace
-
-namespace.setup()
--- d3m/d3m/container/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This module provides various container types one can use to pass values between primitives.
-"""
-
-from .dataset import *
-from .pandas import *
-from .numpy import *
-from .list import *
