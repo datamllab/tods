c730c415a38932361f98111c781f7f767c84de2b
hwy893747147 <hewangyang@yahoo.com>
2021-02-04 16:17:03 -0600

    added examples/sk_examples

---
+++ examples/sk_examples/DeepLog_test.py
@@ -0,0 +1,25 @@
+import numpy as np
+from tods.tods_skinterface.primitiveSKI.detection_algorithm.DeepLog_skinterface import DeepLogSKI
+
+#prepare the data
+
+data = np.loadtxt("./500_UCR_Anomaly_robotDOG1_10000_19280_19360.txt")
+
+# print("shape:", data.shape)
+# print("datatype of data:",data.dtype)
+# print("First 5 rows:\n", data[:5])
+
+X_train = np.expand_dims(data[:10000], axis=1)
+X_test = np.expand_dims(data[10001:], axis=1)
+
+# print("First 5 rows train:\n", X_train[:5])
+# print("First 5 rows test:\n", X_test[:5])
+
+transformer = DeepLogSKI()
+transformer.fit(X_train)
+prediction_labels = transformer.predict(X_test)
+prediction_score = transformer.predict_score(X_test)
+
+print("Primitive: ", transformer.primitive)
+print("Prediction Labels\n", prediction_labels)
+print("Prediction Score\n", prediction_score)
