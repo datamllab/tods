dd4f1dd1131ce7efa44a4f22e464bdd81c16ad20
hwy893747147 <hewangyang@yahoo.com>
2021-02-14 21:44:11 -0600

    trying to output performance, not done.

--- examples/sk_examples/DeepLog_test.py
+++ examples/sk_examples/DeepLog_test.py
@@ -1,5 +1,10 @@
 import numpy as np
 from tods.tods_skinterface.primitiveSKI.detection_algorithm.DeepLog_skinterface import DeepLogSKI
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
 
 #prepare the data
 
@@ -23,3 +28,13 @@ prediction_score = transformer.predict_score(X_test)
 print("Primitive: ", transformer.primitive)
 print("Prediction Labels\n", prediction_labels)
 print("Prediction Score\n", prediction_score)
+
+print('Accuracy Score: ', accuracy_score(X_test, X_train))
+confusion_matrix(X_test, X_train)
+print(classification_report(X_test, X_train))
+
+precision, recall, thresholds = precision_recall_curve(X_test, X_train)
+
+f1_scores = 2*recall*precision/(recall+precision)
+print('Best threshold: ', thresholds[np.argmax(f1_scores)])
+print('Best F1-Score: ', np.max(f1_scores))
