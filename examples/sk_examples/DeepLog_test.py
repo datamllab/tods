92a62bd8987a9e506b5511d5a67f6aea47bac3bc
hwy893747147 <hewangyang@yahoo.com>
2021-02-21 22:55:31 -0600

    Update DeepLog_test.py

--- examples/sk_examples/DeepLog_test.py
+++ examples/sk_examples/DeepLog_test.py
@@ -5,20 +5,12 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
-
 #prepare the data
 
 data = np.loadtxt("./500_UCR_Anomaly_robotDOG1_10000_19280_19360.txt")
 
-# print("shape:", data.shape)
-# print("datatype of data:",data.dtype)
-# print("First 5 rows:\n", data[:5])
-
 X_train = np.expand_dims(data[:10000], axis=1)
-X_test = np.expand_dims(data[10001:], axis=1)
-
-# print("First 5 rows train:\n", X_train[:5])
-# print("First 5 rows test:\n", X_test[:5])
+X_test = np.expand_dims(data[10000:], axis=1)
 
 transformer = DeepLogSKI()
 transformer.fit(X_train)
@@ -31,9 +23,11 @@ prediction_score = transformer.predict_score(X_test)
 print("Primitive: ", transformer.primitive)
 print("Prediction Labels\n", prediction_labels)
 print("Prediction Score\n", prediction_score)
-#np.argmax(X_train, axis=1)
-y_true = prediction_labels_train[:1000]
-y_pred = prediction_labels[:1000]
+
+# y_true = prediction_labels_train[:1000]
+# y_pred = prediction_labels[:1000]
+y_true = prediction_labels_train
+y_pred = prediction_labels
 
 print('Accuracy Score: ', accuracy_score(y_true, y_pred))
 confusion_matrix(y_true, y_pred)
