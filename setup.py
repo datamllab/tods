650f68b16b5377e842efc5ef09508875dfc9359f
lhenry15 <khlai037@gmail.com>
2022-01-19 15:37:27 -0600

    fix task description problem for generate_problem

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
                  ]
     },
     install_requires=[
-        'tamu_d3m==2021.11.24',
+        'tamu_d3m==2022.01.19',
         'tamu_axolotl',
         'numpy<=1.21.2',
         'combo',
