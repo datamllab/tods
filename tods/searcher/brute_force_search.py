c03ec7954a06ad0c3197071c7142cefa9eb50f38
lhenry15 <khlai037@gmail.com>
2022-01-20 14:46:23 -0600

    fix task description problem for generate_problem and brute_force_searcher warning

--- tods/searcher/brute_force_search.py
+++ tods/searcher/brute_force_search.py
@@ -192,19 +192,19 @@ def _generate_pipline(combinations): # pragma: no cover
         # The first three steps are fixed
         # Step 0: dataset_to_dataframe
         step_0 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.tods.data_processing.dataset_to_dataframe'))
-        step_0.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.0')
+        step_0.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data='inputs.0')
         step_0.add_output('produce')
         pipeline_description.add_step(step_0)
 
         # Step 1: column_parser
         step_1 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.tods.data_processing.column_parser'))
-        step_1.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
+        step_1.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data='steps.0.produce')
         step_1.add_output('produce')
         pipeline_description.add_step(step_1)
 
         # Step 2: extract_columns_by_semantic_types(attributes)
         step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.tods.data_processing.extract_columns_by_semantic_types'))
-        step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+        step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data='steps.1.produce')
         step_2.add_output('produce')
         step_2.add_hyperparameter(name='semantic_types', argument_type=ArgumentType.VALUE,
                                                                   data=['https://metadata.datadrivendiscovery.org/types/Attribute'])
@@ -212,7 +212,7 @@ def _generate_pipline(combinations): # pragma: no cover
 
         # Step 3: extract_columns_by_semantic_types(targets)
         step_3 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.tods.data_processing.extract_columns_by_semantic_types'))
-        step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
+        step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data='steps.0.produce')
         step_3.add_output('produce')
         step_3.add_hyperparameter(name='semantic_types', argument_type=ArgumentType.VALUE,
                                                                 data=['https://metadata.datadrivendiscovery.org/types/TrueTarget'])
@@ -222,30 +222,30 @@ def _generate_pipline(combinations): # pragma: no cover
         targets = 'steps.3.produce'
 
         tods_step_4 = PrimitiveStep(primitive=index.get_primitive(combination[0]))
-        tods_step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference=attributes)
+        tods_step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data=attributes)
         tods_step_4.add_output('produce')
         pipeline_description.add_step(tods_step_4)
 
         tods_step_5 = PrimitiveStep(primitive=index.get_primitive(combination[1]))
-        tods_step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.4.produce')
+        tods_step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data='steps.4.produce')
         tods_step_5.add_output('produce')
         pipeline_description.add_step(tods_step_5)
 
         tods_step_6= PrimitiveStep(primitive=index.get_primitive(combination[2]))
-        tods_step_6.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
+        tods_step_6.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data='steps.5.produce')
         tods_step_6.add_output('produce')
         tods_step_6.add_hyperparameter(name='contamination', argument_type=ArgumentType.VALUE, data=combination[3])
         pipeline_description.add_step(tods_step_6)
 
         #tods_step_7 = PrimitiveStep(primitive=index.get_primitive(combination[3]))
-        #tods_step_7.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.6.produce')
+        #tods_step_7.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data='steps.6.produce')
         #tods_step_7.add_output('produce')
         #pipeline_description.add_step(tods_step_7)
 
         # Finalize the pipeline
         final_step = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.tods.data_processing.construct_predictions'))
-        final_step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.6.produce')
-        final_step.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+        final_step.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data='steps.6.produce')
+        final_step.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data='steps.1.produce')
         final_step.add_output('produce')
         pipeline_description.add_step(final_step)
 
