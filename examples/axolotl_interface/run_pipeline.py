6568572de52c0cc08c174d01179cdacdd269d0d6
jjjzy <yujiazhenn@hotmail.com>
2022-06-06 22:23:54 -0500

    test

--- examples/axolotl_interface/run_pipeline.py
+++ examples/axolotl_interface/run_pipeline.py
@@ -35,9 +35,6 @@ pipeline = load_pipeline(pipeline_path)
 
 # Run the pipeline
 pipeline_result = evaluate_pipeline(dataset, pipeline, metric)
-print(list(pd.DataFrame(pipeline_result.outputs[0]['outputs.0'].select_columns([1]))))
-
-
-print(pipeline_result.outputs[0])
+print(pipeline_result.scores)
 #raise pipeline_result.error[0]
 
