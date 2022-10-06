f6627aac5d95a351651699b75f65ee051d5272aa
Wangyang He <hewangyang@yahoo.com>
2022-10-05 22:12:35 -0500

    fix sk examples

--- examples/sk_examples/DeepLog_test.py
+++ examples/sk_examples/DeepLog_test.py
@@ -10,9 +10,17 @@ from sklearn import metrics
 #prepare the data
 data = np.loadtxt("./500_UCR_Anomaly_robotDOG1_10000_19280_19360.txt")
 
+# first 10000 data points are training data
 X_train = np.expand_dims(data[:10000], axis=1)
-X_test = np.expand_dims(data[10000:], axis=1)
+# second 10000 data points are testing data
+X_test = np.expand_dims(data[10000:20000], axis=1)
 
+# creating labels
+y_test = np.zeros(10000).astype(np.int32)
+y_test[9280:9360] = 1
+y_test = np.expand_dims(y_test, axis=1)
+
+# TODS pipeline
 transformer = DeepLogSKI()
 transformer.fit(X_train)
 
@@ -24,10 +32,12 @@ prediction_score = transformer.predict_score(X_test)
 print("Prediction Labels\n", prediction_labels)
 print("Prediction Score\n", prediction_score)
 
-# y_true = prediction_labels_train[:1000]
-# y_pred = prediction_labels[:1000]
-y_true = prediction_labels_train
+# set y_true to ground truth label information
+y_true = y_test
+# set y_pred to X_test pred labels
 y_pred = prediction_labels
+
+print("Check shapes:", y_true.shape, y_pred.shape)
 
 print('Accuracy Score: ', accuracy_score(y_true, y_pred))
 
@@ -36,6 +46,7 @@ confusion_matrix(y_true, y_pred)
 print(classification_report(y_true, y_pred))
 
 precision, recall, thresholds = precision_recall_curve(y_true, y_pred)
+np.seterr(invalid='ignore')
 f1_scores = 2*recall*precision/(recall+precision)
 
 print('Best threshold: ', thresholds[np.argmax(f1_scores)])
--- examples/sk_examples/Telemanom_test.py
+++ examples/sk_examples/Telemanom_test.py
@@ -6,29 +6,43 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 from sklearn import metrics
+
 #prepare the data
-
 data = np.loadtxt("./500_UCR_Anomaly_robotDOG1_10000_19280_19360.txt")
-# print("shape:", data.shape)
-# print("datatype of data:",data.dtype)
-# print("First 5 rows:\n", data[:5])
 
+# first 10000 data points are training data
 X_train = np.expand_dims(data[:10000], axis=1)
-X_test = np.expand_dims(data[10000:], axis=1)
+# second 10000 data points are testing data
+X_test = np.expand_dims(data[10000:20000], axis=1)
 
-# print("First 5 rows train:\n", X_train[:5])
-# print("First 5 rows test:\n", X_test[:5])
+# creating labels
+y_test = np.zeros(10000).astype(np.int32)
+y_test[9280:9360] = 1
+y_test = np.expand_dims(y_test, axis=1)
 
+# TODS pipeline
 transformer = TelemanomSKI(l_s= 2, n_predictions= 1)
 transformer.fit(X_train)
+
 prediction_labels_train = transformer.predict(X_train)
+
 prediction_labels = transformer.predict(X_test)
 prediction_score = transformer.predict_score(X_test)
 
 print("Prediction Labels\n", prediction_labels)
 print("Prediction Score\n", prediction_score)
-y_true = prediction_labels_train
+
+# set y_true to ground truth label information
+y_true = y_test
+# set y_pred to X_test pred labels
 y_pred = prediction_labels
+
+# match the lable shape with output prediction shape
+#TODO
+pred_shape = y_pred.shape[0]
+y_true = y_true[:pred_shape]
+
+print("Check shapes:", y_true.shape, y_pred.shape)
 
 print('Accuracy Score: ', accuracy_score(y_true, y_pred))
 
@@ -37,6 +51,7 @@ confusion_matrix(y_true, y_pred)
 print(classification_report(y_true, y_pred))
 
 precision, recall, thresholds = precision_recall_curve(y_true, y_pred)
+np.seterr(invalid='ignore')
 f1_scores = 2*recall*precision/(recall+precision)
 
 print('Best threshold: ', thresholds[np.argmax(f1_scores)])
