8c9c25eb1d9bbfb60f77be761fa5633fe474c4cf
lhenry15 <khlai037@gmail.com>
2022-11-21 15:41:48 -0600

    fix unit-tests

--- tods/tests/detection_algorithm/test_PyodSoGaal.py
+++ tods/tests/detection_algorithm/test_PyodSoGaal.py
@@ -19,10 +19,10 @@ class PyodSoGaalTestCase(unittest.TestCase):
         self.n_test = 100
         self.contamination = 0.1
         self.roc_floor = -1e-5
-        self.X_train, self.X_test, self.y_train, self.y_test = generate_data(
+
+        self.X_train, self.y_train, self.X_test, self.y_test = generate_data(
             n_train=self.n_train, n_test=self.n_test,
             contamination=self.contamination, random_state=42)
-
         self.X_train = d3m_dataframe(self.X_train, generate_metadata=True)
         self.X_test = d3m_dataframe(self.X_test, generate_metadata=True)
 
